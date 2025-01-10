8d4aa42c4e265da52ef2fd65b43d3359975c3b23
Nestor Júnior <contato.nestor@gmail.com>
2025-01-10 15:19:08 -0300

    feat: create logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <>
       <header className="flex items-center justify-between h-28">
-        <Image src="/logo.svg" alt="logo" width={166} height={46}></Image>
+        <Image src="/logo.svg" alt="logo" width={50} height={50}></Image>
         <Button />
       </header>
     </>
